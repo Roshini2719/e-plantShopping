6f07338d4a4e9578dfae605195e9b617f1d8379c
Roshini2719 <muthamilroshinis@gmail.com>
2026-09-22 15:56:15 +0530

    Add shopping cart item component

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,68 @@
+import { useDispatch } from 'react-redux'
+import { removeItem, updateQuantity } from '../redux/CartSlice'
+
+function CartItem({ item }) {
+  const dispatch = useDispatch()
+
+  const increaseQuantity = () => {
+    dispatch(
+      updateQuantity({
+        id: item.id,
+        quantity: item.quantity + 1,
+      })
+    )
+  }
+
+  const decreaseQuantity = () => {
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({
+          id: item.id,
+          quantity: item.quantity - 1,
+        })
+      )
+    }
+  }
+
+  const removeFromCart = () => {
+    dispatch(removeItem(item.id))
+  }
+
+  return (
+    <div className="cart-item">
+      <img
+        src={item.image}
+        alt={item.name}
+        className="cart-item-image"
+      />
+
+      <div className="cart-item-details">
+        <h3>{item.name}</h3>
+        <p>Category: {item.category}</p>
+        <p>Price: ${item.price}</p>
+
+        <div className="quantity-controls">
+          <button type="button" onClick={decreaseQuantity}>
+            -
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button type="button" onClick={increaseQuantity}>
+            +
+          </button>
+        </div>
+
+        <p>
+          Subtotal: ${(item.price * item.quantity).toFixed(2)}
+        </p>
+
+        <button type="button" onClick={removeFromCart}>
+          Remove
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
