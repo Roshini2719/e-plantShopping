105e6b52cc62a3c94424efb41e2c8e46db2f3e22
Roshini2719 <muthamilroshinis@gmail.com>
2026-09-22 15:43:05 +0530

    Add Paradise Nursery React project

---
+++ src/AboutUs.jsx
@@ -0,0 +1,25 @@
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is an online plant shop dedicated to bringing
+        beautiful and healthy houseplants to plant lovers.
+      </p>
+
+      <p>
+        We offer a variety of indoor plants for homes, offices, and
+        personal spaces. Our goal is to make it easy for customers to
+        discover, choose, and purchase plants online.
+      </p>
+
+      <p>
+        At Paradise Nursery, we believe that plants can make spaces
+        more beautiful, peaceful, and refreshing.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
